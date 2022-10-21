679b7d1c0f0e07c45b291a84f1d9a07be094556d
Kyle Spaans <kyle@spaans.ca>
2022-10-21 13:21:35 -0400

    WIP

--- api/index.js
+++ api/index.js
@@ -6,27 +6,34 @@ app.use(express.static('public'));
 const YOUR_DOMAIN = 'http://localhost:4242';
 
 app.post('/api/create-checkout-session', async (req, res) => {
-  const session = await stripe.checkout.sessions.create({
-    line_items: [
-      {
-        // Provide the exact Price ID (for example, pr_1234) of the product you want to sell
-        price: 'price_1Kf7PCAlFwVdzE6LVkv49oka',
-        quantity: 1,
+  let body = [];
+  req.on('data', (chunk) => {
+    body.push(chunk);
+  }).on('end', async () => {
+    body = Buffer.concat(body).toString();
+    const product = body.split('=')[1];
+    const session = await stripe.checkout.sessions.create({
+      line_items: [
+        {
+          price: product,
+          quantity: 1,
+          adjustable_quantity: { enabled: true },
+        },
+      ],
+      mode: 'payment',
+      success_url: 'https://roast.spaans.ca/success.html',
+      cancel_url:  'https://roast.spaans.ca/cancel.html',
+      shipping_address_collection: {
+        allowed_countries: ['CA'],
       },
-    ],
-    mode: 'payment',
-    success_url: 'https://roast.spaans.ca/success.html',
-    cancel_url:  'https://roast.spaans.ca/cancel.html',
-    shipping_address_collection: {
-      allowed_countries: ['CA'],
-    },
-    shipping_options: [
-      { shipping_rate: 'shr_1Kf7VPAlFwVdzE6LS76d1Gdn' },
-      { shipping_rate: 'shr_1Kf7WCAlFwVdzE6LmlHhPgGN' },
-    ],
-  });
+      shipping_options: [
+        { shipping_rate: 'shr_1Kf7VPAlFwVdzE6LS76d1Gdn' },
+        { shipping_rate: 'shr_1Kf7WCAlFwVdzE6LmlHhPgGN' },
+      ],
+    });
 
-  res.redirect(303, session.url);
+    res.redirect(303, session.url);
+  });
 });
 
 app.listen(4242, () => console.log('Running on port 4242'));
